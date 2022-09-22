c0cac4138bba7530399d5135fce3818714f722dc
+ <+>
2022-09-22 14:21:53 -0400

    update migrations for heroku deployment

--- db/migrate/20220922172815_add_user_info_to_users.rb
+++ db/migrate/20220922172815_add_user_info_to_users.rb
@@ -1,8 +1,8 @@
 class AddUserInfoToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :about_me, :string
-    add_column :users, :profile_pic, :string
+    add_column :users, :firstName, :string
+    add_column :users, :lastName, :string
+    add_column :users, :aboutMe, :string
+    add_column :users, :profilePic, :string
   end
 end
