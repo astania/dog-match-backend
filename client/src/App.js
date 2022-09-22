22f438d40938b1c41ed6466a6e8e6a3f9eb8b9e3
+ <+>
2022-09-22 14:52:52 -0400

    add preliminary header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Login from "./login_components/Login";
 import WelcomePage from "./login_components/WelcomePage";
+import Footer from "./navigation_components/Footer";
+import Header from "./navigation_components/Header";
 
 function App() {
  
@@ -32,12 +34,13 @@ function App() {
 
   return (
     <BrowserRouter>
-
+    <Header />
       <Routes>
         <Route exact path="/" element={ loggedIn ? <WelcomePage user={user} onLogout={onLogout} /> : <Login onLogin={onLogin} /> } />
         <Route exact path="/login" element={ <Login onLogin={onLogin} />} />
         <Route exact path="/testing" element={<h1>Test Route</h1>} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ client/src/navigation_components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h3>DogMatch</h3>
+        <h5><em>Find playmates for your best friend</em></h5>
+
+    </div>
+  )
+}
+
+export default Header
