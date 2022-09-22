8a5b435a961d6bc63e963a9b72e46d9158cf0060
+ <+>
2022-09-22 14:28:35 -0400

    update migrations for heroku deployment

--- db/migrate/20220920151350_add_password_digest_to_users.rb
+++ db/migrate/20220920151350_add_password_digest_to_users.rb
@@ -1,5 +1,9 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :password_digest, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :about_me, :string
+    add_column :users, :profile_pic, :string
   end
 end
--- db/migrate/20220922172815_add_user_info_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddUserInfoToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :firstName, :string
-    add_column :users, :lastName, :string
-    add_column :users, :aboutMe, :string
-    add_column :users, :profilePic, :string
-  end
-end
