acd6f73546939dc8b7bf1b5c632937f1efb75741
+ <+>
2022-09-29 11:56:57 -0400

    update dog serializer to exclude id, user_id

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -1,4 +1,4 @@
 class DogSerializer < ActiveModel::Serializer
-  attributes :id, :name, :breed, :user_id, :profile_pic, :about_me
+  attributes :name, :breed, :profile_pic, :about_me
   belongs_to :user
 end
