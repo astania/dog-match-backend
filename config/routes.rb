8e4d614fd5b228e10bd12e031027c965b3a7c88a
+ <+>
2022-10-25 18:44:52 -0400

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,16 @@
 Rails.application.routes.draw do
-  resources :playdate_requested_dogs
-  resources :playdates
+  resources :playdate_requested_dogs, only: [:create, :destroy]
+  resources :playdates, only: [:create, :destroy]
   
-  resources :dogs, only: [:create, :show, :index, :destroy]
+  resources :dogs, only: [:create, :show, :index, :destroy, :update]
 
-  resources :users, only: [:create, :destroy]
+  resources :users, only: [:create, :destroy, :update]
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
-  get '/users/:id', to: 'users#show'
   
   get '/me', to: 'users#show'
-  
-  patch '/users/:id', to: 'users#update'
 
-  patch 'dogs/:id', to: 'dogs#update'
-  
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
